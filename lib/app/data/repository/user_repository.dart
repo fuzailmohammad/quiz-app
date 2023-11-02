d0f0b110bfe3c7635d9598d336c07b5ef7064fdb
fuzailmohammad <mohdfuzail@outlook.com>
2023-11-02 10:24:05 +0530

    unknown

--- lib/app/data/repository/user_repository.dart
+++ lib/app/data/repository/user_repository.dart
@@ -28,7 +28,7 @@ class UserRepository extends BaseRepository {
   Future<RepoResponse<User>> signUp(SignUpRequest data) async {
     final response =
         await controller.post(path: URLs.signUp, data: data.toJson());
-
+    
     return response is APIException
         ? RepoResponse(error: response)
         : RepoResponse(data: UserResponse.fromJson(response).data);
